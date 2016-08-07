7881e30af5d7783f5fe7dbe6f1744528dc185237
JD Panzer <jason.drew.panzer@gmail.com>
2016-08-06 23:57:38 -0700

    initiative: expanded on functional tests to describe desired init tracker behavior

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,32 +20,57 @@ class NewVisitorTest(unittest.TestCase):
 
 		# The page title reads 'Critaholic'
 		self.assertIn('Critaholic', self.browser.title)
-		self.fail('Finish the test!')
 
 		# and gives a brief description of how to use it
-		# assertIn('Instruction line', self.browser.something?)
+		instruction_text = self.browser.find_element_by_id('instructions')
+		self.assertIn('First, enter the name of the monster.', instruction_text)
+		self.assertIn('Second, enter the initiative.', instruction_text)
 
 		# She is invited to enter the name of a PC or monster and its initiative
 		# She types in 'displacer beast' and '10'
-		# There's a keys command or somesuch.
+		# find the input box for monster name
+		initiative_name_input = self.browser.find_element_by_id('initiative_name_input')
+		initiative_name_input.send_keys('Displacer beast')
+		# find the input box for the initiative
+		initiative_number_input = self.browser.find_element_by_id('initiative_number_input')
+		initiative_number_input.send_keys('10')
+		# Enter 
+		initiative_number_input.send_keys(Keys.ENTER)
 
 		# When she hits enter the page updates and she sees 10 and the name of 
 		# the displacer beast
+		# assert that the words displacer beast and the number ten are in 
+		# a row or cell that match the right id.
+		name_display_cells = self.browser.find_elements_by_name('initiative_name_display')
+		init_display_cells = self.browser.find_elements_by_name('initiative_number_display')
+		self.assertTrue(any(name_display_cells.text == 'Displacer Beast'))
+		self.assertTrue(any(init_display_cells.text == '10'))
+		self.fail('Finish the test!')
 
 		# There is still a pair of text boxes ready to accept information
-		# Gina types in 'ettin' and '2'. She hits enter
+		# assert that the form fields are there
 
-		# And the page updates again, showing both creatures (though not sorted
-		# by initiative)
+		# Gina types in 'ettin' and '2'. She hits enter
+		# send keys again
+
+
+		# And the page updates again, showing both creatures (sorted by initiative)
+		# iterate through the cells and rows
+		#	assert that the appropriate beasts are listed in the appropriate order
+
 
 		# Gina ends her session, but wonders if the site will remember this enc-
 		# ounter's initiative order. She sees that the site has generated a uni-
 		# que URL for her.
+		# assert something about the url
 
 		# She visits that URL and, sure enough, the list is there, waiting for 
 		# her.
+		# browser gets the unique url
+		# test that the things we expect are displayed
 
 		# Satisfied, she carries on with the session.
+		# end test one.
 
 		# Second tier project goals
 		# 	Add hit points to the monsters

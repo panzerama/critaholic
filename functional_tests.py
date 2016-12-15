27df9b2ffb3118aa4af4f94239a07cdc41a21f95
JD Panzer <jason.drew.panzer@gmail.com>
2016-12-15 13:45:19 -0800

    PEP 8 standards changes

--- functional_tests.py
+++ functional_tests.py
@@ -4,126 +4,126 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Chrome()
-		self.browser.implicitly_wait(3)
+    def setUp(self):
+        self.browser = webdriver.Chrome('/Users/jd/Resources/chromedriver')
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def check_for_cells_in_list_table(self, cell_text):
-		table = self.browser.find_element_by_id('init_table')
-		cells = table.find_elements_by_tag_name('td')
-		self.assertIn(cell_text, [cell.text for cell in cells])
+    def check_for_cells_in_list_table(self, cell_text):
+        table = self.browser.find_element_by_id('init_table')
+        cells = table.find_elements_by_tag_name('td')
+        self.assertIn(cell_text, [cell.text for cell in cells])
 
-	def test_new_user_can_create_initiative(self):
-		# Gina the GM has heard about a fantastic new app for tracking 
-		# initiative. She decides to try it out. She opens a browser and 
-		# navigates to the page.
-		self.browser.get('http://localhost:8000')
+    def test_new_user_can_create_initiative(self):
+        # Gina the GM has heard about a fantastic new app for tracking 
+        # initiative. She decides to try it out. She opens a browser and 
+        # navigates to the page.
+        self.browser.get('http://localhost:8000')
 
-		# The page title reads 'Critaholic'
-		self.assertIn('Critaholic', self.browser.title)
+        # The page title reads 'Critaholic'
+        self.assertIn('Critaholic', self.browser.title)
 
-		# and gives a brief description of how to use it
-		instruction_text = self.browser.find_element_by_id('instructions')
-		self.assertIn('First, enter the name of the monster.', instruction_text.text)
-		self.assertIn('Second, enter the initiative.', instruction_text.text)
+        # and gives a brief description of how to use it
+        instruction_text = self.browser.find_element_by_id('instructions')
+        self.assertIn('First, enter the name of the monster.', instruction_text.text)
+        self.assertIn('Second, enter the initiative.', instruction_text.text)
 
-		# She is invited to enter the name of a PC or monster and its initiative
-		initiative_name_input = self.browser.find_element_by_id('initiative_name_input')
-		self.assertEqual(
-			initiative_name_input.get_attribute('placeholder'),
-			'Enter a monster\'s name'
-		)
+        # She is invited to enter the name of a PC or monster and its initiative
+        initiative_name_input = self.browser.find_element_by_id('initiative_name_input')
+        self.assertEqual(
+            initiative_name_input.get_attribute('placeholder'),
+            'Enter a monster\'s name'
+        )
 
-		# She types in 'displacer beast' and '10'
-		initiative_name_input.send_keys('Displacer beast')
+        # She types in 'displacer beast' and '10'
+        initiative_name_input.send_keys('Displacer beast')
 
-		# find the input box for the initiative
-		initiative_number_input = self.browser.find_element_by_id('initiative_number_input')
-		self.assertEqual(
-			initiative_number_input.get_attribute('placeholder'),
-			'Enter the monster\'s initiative'
-		)
-		initiative_number_input.send_keys('10')
+        # find the input box for the initiative
+        initiative_number_input = self.browser.find_element_by_id('initiative_number_input')
+        self.assertEqual(
+            initiative_number_input.get_attribute('placeholder'),
+            'Enter the monster\'s initiative'
+        )
+        initiative_number_input.send_keys('10')
 
-		# Enter
-		initiative_submit = self.browser.find_element_by_id('initiative_submit')
-		initiative_submit.click()
+        # Enter
+        initiative_submit = self.browser.find_element_by_id('initiative_submit')
+        initiative_submit.click()
 
-		# When she hits enter the page updates and she sees 10 and the name of 
-		# the displacer beast
-		# assert that the words displacer beast and the number ten are in 
-		# a row or cell that match the right id.
+        # When she hits enter the page updates and she sees 10 and the name of 
+        # the displacer beast
+        # assert that the words displacer beast and the number ten are in 
+        # a row or cell that match the right id.
 
-		self.check_for_cells_in_list_table('Displacer beast')
-		self.check_for_cells_in_list_table('10')
+        self.check_for_cells_in_list_table('Displacer beast')
+        self.check_for_cells_in_list_table('10')
 
-		# There is still a pair of text boxes ready to accept information
-		# assert that the form fields are there
-		initiative_name_input = self.browser.find_element_by_id('initiative_name_input')
-		self.assertEqual(
-			initiative_name_input.get_attribute('placeholder'),
-			'Enter a monster\'s name'
-		)
+        # There is still a pair of text boxes ready to accept information
+        # assert that the form fields are there
+        initiative_name_input = self.browser.find_element_by_id('initiative_name_input')
+        self.assertEqual(
+            initiative_name_input.get_attribute('placeholder'),
+            'Enter a monster\'s name'
+        )
 
-		# She types in 'ettin
-		initiative_name_input.send_keys('Ettin')
+        # She types in 'ettin
+        initiative_name_input.send_keys('Ettin')
 
-		# find the input box for the initiative
-		initiative_number_input = self.browser.find_element_by_id('initiative_number_input')
-		self.assertEqual(
-			initiative_number_input.get_attribute('placeholder'),
-			'Enter the monster\'s initiative'
-		)
+        # find the input box for the initiative
+        initiative_number_input = self.browser.find_element_by_id('initiative_number_input')
+        self.assertEqual(
+            initiative_number_input.get_attribute('placeholder'),
+            'Enter the monster\'s initiative'
+        )
 
-		# She types in '2', ettins not being known for their high dex
-		initiative_number_input.send_keys('2')
+        # She types in '2', ettins not being known for their high dex
+        initiative_number_input.send_keys('2')
 
-		# And she clicks 'submit'
-		initiative_submit = self.browser.find_element_by_id('initiative_submit')
-		initiative_submit.click()
-
-
-		# And the page updates again, showing both creatures (sorted by initiative)
-		# iterate through the cells and rows
-		#	assert that the appropriate beasts are listed in the appropriate order
-		# 	TODO does selenium return things in order
-
-		self.check_for_cells_in_list_table('Displacer beast')
-		self.check_for_cells_in_list_table('10')
-		self.check_for_cells_in_list_table('Ettin')
-		self.check_for_cells_in_list_table('2')
-
-		self.fail('Finish the test!')
+        # And she clicks 'submit'
+        initiative_submit = self.browser.find_element_by_id('initiative_submit')
+        initiative_submit.click()
 
 
-		# Gina ends her session, but wonders if the site will remember this enc-
-		# ounter's initiative order. She sees that the site has generated a uni-
-		# que URL for her.
-		# assert something about the url
+        # And the page updates again, showing both creatures (sorted by initiative)
+        # iterate through the cells and rows
+        #    assert that the appropriate beasts are listed in the appropriate order
+        #     TODO does selenium return things in order
 
-		# She visits that URL and, sure enough, the list is there, waiting for 
-		# her.
-		# browser gets the unique url
-		# test that the things we expect are displayed
+        self.check_for_cells_in_list_table('Displacer beast')
+        self.check_for_cells_in_list_table('10')
+        self.check_for_cells_in_list_table('Ettin')
+        self.check_for_cells_in_list_table('2')
 
-		# Satisfied, she carries on with the session.
-		# end test one.
+        self.fail('Finish the test!')
 
-		# Second tier project goals
-		# 	Add hit points to the monsters
-		#	Let the user change the hit points
-		# 	Automatically sort the list
-		#	Delete monsters
-		# Third tier project goals
-		#	drag and drop items to reorganize
-		#	create multiple monsters
-		# Future
-		# 	Mobile responsive design
-		# Other projects
-		#	an app for tracking distances between creatures
+
+        # Gina ends her session, but wonders if the site will remember this enc-
+        # ounter's initiative order. She sees that the site has generated a uni-
+        # que URL for her.
+        # assert something about the url
+
+        # She visits that URL and, sure enough, the list is there, waiting for 
+        # her.
+        # browser gets the unique url
+        # test that the things we expect are displayed
+
+        # Satisfied, she carries on with the session.
+        # end test one.
+
+        # Second tier project goals
+        #     Add hit points to the monsters
+        #    Let the user change the hit points
+        #     Automatically sort the list
+        #    Delete monsters
+        # Third tier project goals
+        #    drag and drop items to reorganize
+        #    create multiple monsters
+        # Future
+        #     Mobile responsive design
+        # Other projects
+        #    an app for tracking distances between creatures
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- initiative/tests.py
+++ initiative/tests.py
@@ -3,65 +3,62 @@ from django.shortcuts import render
 from django.test import TestCase
 from django.http import HttpRequest
 from initiative.views import home_page
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 from initiative.models import Initiative
+
 
 class HomePageTest(TestCase):
 
-	#TODO later on, change the home page to a menu system and move this to '/init'
-	def test_root_url_resolves_to_initiative_tracker(self): 
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+    # TODO later on, change the home page to a menu system and move this to '/init'
+    def test_root_url_resolves_to_initiative_tracker(self): 
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-	#testing csrf tokens is difficult in 1.10, testing only a part of the pages
-	def test_home_page_shows_correct_html(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['init_name'] = 'beholder'
-		request.POST['init_num'] = 18
-		response = home_page(request)
-		expected_output = render(request, 'home.html')
-		self.assertEqual(response.content.decode()[:40], expected_output.content.decode()[:40])
+    # testing csrf tokens is difficult in 1.10, testing only a part of the pages
+    def test_home_page_shows_correct_html(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['init_name'] = 'beholder'
+        request.POST['init_num'] = 18
+        response = home_page(request)
+        expected_output = render(request, 'home.html')
+        self.assertEqual(response.content.decode()[:40], expected_output.content.decode()[:40])
 
-	#fix the lack of CSRF testing by implementing django.test.Client?
-	def test_name_and_init_input_saved_by_POST(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['init_name'] = 'beholder'
-		request.POST['init_num'] = 18
-		response = home_page(request)
+    # fix the lack of CSRF testing by implementing django.test.Client?
+    def test_name_and_init_input_saved_by_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['init_name'] = 'beholder'
+        request.POST['init_num'] = 18
+        response = home_page(request)
 
-		self.assertEqual(Initiative.objects.count(), 1)
-		new_init_order = Initiative.objects.first()
-		self.assertEqual(new_init_order.creature_name, 'beholder')
-		self.assertEqual(new_init_order.initiative_value, 18)
+        self.assertEqual(Initiative.objects.count(), 1)
+        new_init_order = Initiative.objects.first()
+        self.assertEqual(new_init_order.creature_name, 'beholder')
+        self.assertEqual(new_init_order.initiative_value, 18)
 
-		self.assertIn('beholder', response.content.decode())
-		self.assertIn('18', response.content.decode())
+        self.assertIn('beholder', response.content.decode())
+        self.assertIn('18', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
-	def test_save_initiative_object_and_retrieve(self):
-		first_init = Initiative()
-		first_init.creature_name = 'Shaltorin'
-		first_init.initiative_value = 1
-		first_init.save()
+    def test_save_initiative_object_and_retrieve(self):
+        first_init = Initiative()
+        first_init.creature_name = 'Shaltorin'
+        first_init.initiative_value = 1
+        first_init.save()
 
-		second_init = Initiative()
-		second_init.creature_name = 'Falkrainne'
-		second_init.initiative_value = 20
-		second_init.save()
+        second_init = Initiative()
+        second_init.creature_name = 'Falkrainne'
+        second_init.initiative_value = 20
+        second_init.save()
 
-		init_order = Initiative.objects.all()
-		self.assertEqual(init_order.count(), 2)
+        init_order = Initiative.objects.all()
+        self.assertEqual(init_order.count(), 2)
 
-		first_saved_init = init_order[0]
-		second_saved_init = init_order[1]
+        first_saved_init = init_order[0]
+        second_saved_init = init_order[1]
 
-		self.assertEqual(first_saved_init, first_init)
-		self.assertEqual(second_saved_init, second_init)
-
-
-
-
-
+        self.assertEqual(first_saved_init, first_init)
+        self.assertEqual(second_saved_init, second_init)

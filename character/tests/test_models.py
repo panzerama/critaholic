3cbd5e0826004f7c8cb18727bb5f35de40bf74db
JD Panzer <jason.drew.panzer@gmail.com>
2017-01-16 12:50:42 -0800

    placeholder for first character test

---
+++ character/tests/test_models.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+# from initiative.models import Initiative, Encounter
+
+
+class CharacterModelTest(TestCase):
+
+    def test_character_model_saves_notes(self):
+        pass

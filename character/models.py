ac6f8faef0f9fd3d35de3cc25d0cf97a3482f9eb
JD Panzer <jason.drew.panzer@gmail.com>
2017-01-16 13:27:11 -0800

    basic character model up

--- character/models.py
+++ character/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    character_name = models.TextField(blank=False, default='NPC')
+    summary = models.TextField(default=' ')
+
+# TODO better default values for character_name

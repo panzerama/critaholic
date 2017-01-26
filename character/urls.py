7b614e889319f609d7177f56acc7ea2785108cc8
JD Panzer <jason.drew.panzer@gmail.com>
2017-01-25 19:43:00 -0800

    fixed routing issue between init and character apps

--- character/urls.py
+++ character/urls.py
@@ -5,5 +5,5 @@ from character import views
 urlpatterns = [
     url(r'^$', views.char_page, name='character home'),
     url(r'^(\d+)/$', views.view_char, name='character view'),
-    url(r'^new$', views.new_char, name='new'),
+    url(r'^new$', views.new_char, name='new char'),
 ]

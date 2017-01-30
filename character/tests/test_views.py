42aff65253949d5e9d1215e6910d5c26da5f91b8
JD Panzer <jason.drew.panzer@gmail.com>
2017-01-29 22:16:39 -0800

    amending character view to accomodate a list of characters

--- character/tests/test_views.py
+++ character/tests/test_views.py
@@ -11,7 +11,7 @@ class NewCharacterTest(TestCase):
 
     def test_character_home_page_uses_correct_template(self):
         response = self.client.get('/character/')
-        self.assertTemplateUsed(response, 'char_home.html')
+        self.assertTemplateUsed(response, 'base.html')
 
     def test_new_character_redirects_after_POST(self):
         response = self.client.post('/character/new', data={'character_name': 'Shaltorinn',
@@ -21,3 +21,12 @@ class NewCharacterTest(TestCase):
 
         new_character = Character.objects.first()
         self.assertEqual(response['location'], '/character/%d/' % (new_character.id,))
+
+    def test_name_and_description_input_saved_by_POST(self):
+        self.client.post('/character/new', data={'character_name': 'Falkrainne',
+                                                 'character_description': 'Manically cheerful, short'})
+
+        self.assertEqual(Character.objects.count(), 1)
+        new_character = Character.objects.first()
+        self.assertEqual(new_character.character_name, 'Falkrainne')
+        self.assertEqual(new_character.summary, 'Manically cheerful, short')
--- character/views.py
+++ character/views.py
@@ -3,11 +3,17 @@ from character.models import Character
 
 
 def char_page(request):
-    return render(request, 'char_home.html')
+    return render(request, 'home.html')
+
 
 def view_char(request, character_id):
-    pass
+    character_ = Character.objects.get(id=character_id)
+    return render(request, 'view_char.html', {'character': character_})
+
 
 def new_char(request):
-    character_ = Character.objects.create()
+    character_ = Character.objects.create(character_name=request.POST['character_name'],
+                                          summary=request.POST['character_description'])
+    character_.full_clean()
+    character_.save()
     return redirect(character_)
